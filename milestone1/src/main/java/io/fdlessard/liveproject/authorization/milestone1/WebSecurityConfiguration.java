9d4b5824ef7b75c0bf956bf3b2e0ed3b9213f54a
fdlessard <fdlessard@gmail.com>
2021-05-28 21:49:40 -0400

    Added modules per milestones

---
+++ milestone1/src/main/java/io/fdlessard/liveproject/authorization/milestone1/WebSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package io.fdlessard.liveproject.authorization.milestone1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
---
+++ milestone2/src/main/java/io/fdlessard/liveproject/authorization/milestone2/WebSecurityConfiguration.java
@@ -0,0 +1,38 @@
+package io.fdlessard.liveproject.authorization.milestone2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var uds = new InMemoryUserDetailsManager();
+        var u = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        uds.createUser(u);
+        return uds;
+    }
+
+}
