b24d26bb2a5c414781e8cf2577bca5115a42defd
fdlessard <fdlessard@gmail.com>
2021-05-31 22:39:59 -0400

    milestone 4

--- milestone4/src/main/java/io/fdlessard/liveproject/authorization/milestone4/configurations/WebSecurityConfiguration.java
+++ milestone4/src/main/java/io/fdlessard/liveproject/authorization/milestone4/configurations/WebSecurityConfiguration.java
@@ -5,11 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -22,19 +19,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean()
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var uds = new InMemoryUserDetailsManager();
-        var u = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        uds.createUser(u);
-        return uds;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
